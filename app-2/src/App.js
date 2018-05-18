e8ef675dee16ffa336bb02732649cb21cb7bbec3
unknown <TaurusSHO89@gmail.com>
2018-05-17 20:53:44 -0600

    app1, 2, and 3 are done

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor () {
+    super();
+    this.state = {
+      listArr: ['Hifumi', 'Glacial', 'Otown', 'Hopeless', 'Anstormning']
+    }
+  }
+
+  listFun = () => {
+    let arr = [];
+    for (let i=0;i<this.state.listArr.length;i++) {
+      arr.push(<li>{this.state.listArr[i]}</li>)
+    }
+
+    return arr
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {this.listFun()}
+        </ul>
       </div>
     );
   }
--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor () {
+    super();
+    this.state = {
+      listArr: ['Hifumi', 'Glacial', 'Hopeless', 'Otown', 'Anstormning'],
+      userInput: ''
+    }
+  }
+
+  arrList = (val) => {
+    let arr = [];
+    for (let i=0;i<this.state.listArr.length;i++) {
+      if (this.state.listArr[i].indexOf(val) > -1) {
+        arr.push(<h2>{this.state.listArr[i]}</h2>)
+      }
+    }
+
+    return arr
+  }
+
+  changeHandler = (val) => {
+    this.setState({userInput: val})
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(e) => this.changeHandler(e.target.value)}></input>
+        {this.arrList(this.state.userInput)}
       </div>
     );
   }
